f4953ec1282649892f501e8a899e5edfa89e8ebd
sam l <sluongo718@yahoo.com>
2020-09-10 18:00:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require "pry"
 class CashRegister
   
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :void_last_transaction
   
   def initialize(discount = 0)
     @total = 0
