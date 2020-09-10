5b60dc09137f30974f72953618d08176b3c7fe3a
sam l <sluongo718@yahoo.com>
2020-09-10 17:55:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,9 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     if quantity > 1
       i = 0 
-      while i < 
+      while i < quantity
     @items << item
+     i += 1
      binding.pry
   end
   
