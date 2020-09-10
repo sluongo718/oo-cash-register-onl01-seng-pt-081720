20c1469d54205336ca2887542e9ef72df743fc05
sam l <sluongo718@yahoo.com>
2020-09-10 17:51:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,8 +11,10 @@ class CashRegister
   end
   
   def add_item(item, price, quantity = 1)
-    
+    if quantity > 1
     @items << item
+     item
+  end
     self.total += price * quantity
   
   end
